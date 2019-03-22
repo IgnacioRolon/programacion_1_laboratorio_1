35c67422a132def8cb49fdec22377d17992f5683
alumno <alumno>
2019-03-21 21:56:09 -0300

    Clase 2

--- Clase_01/Clase_01_E1/main.c
+++ Clase_01/Clase_01_E1/main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <limits.h>
 
 //calcular promedio en una funcion
 
@@ -7,34 +8,82 @@
 
 // int sumarNumeros();
 int calcularPromedio();
+int funcionPromedio(int valorAcumulado, int cantidad, float *promedio);
+int calcularMaximoMinimo(int valorIngresado, int *maximo, int *minimo);
 
 int main()
 {
-
     calcularPromedio();
     return 0;
-
 }
 
 int calcularPromedio()
 {
     int i;
     float promedio;
+    int acumulador = 0;
     int ingreso;
-
-    promedio = 0;
+    int maximo = INT_MIN;
+    int minimo = INT_MAX;
 
     for(i = 0;i < CANTIDAD;i++)
     {
         printf("Ingrese un numero: ");
         scanf("%d", &ingreso);
-        promedio += ingreso;
+        acumulador += ingreso;
+        if(calcularMaximoMinimo(ingreso, &maximo, &minimo))
+        {
+            printf("Error");
+            break;
+        }
     }
-
-    promedio = promedio / CANTIDAD;
-    printf("El promedio es: %f", promedio);
+    //promedio = (float)acumulador / CANTIDAD;
+    if(funcionPromedio(acumulador, CANTIDAD, &promedio) == 0)
+    {
+        printf("\nEl promedio es: %.2f", promedio);
+    }
+    else{
+        printf("\n");
+        printf("\nError");
+    }
+    printf("\nEl maximo es: %d", maximo);
+    printf("\nEl minimo es: %d", minimo);
+    printf("\n");
     return 0;
 }
+
+int funcionPromedio(int valorAcumulado, int cantidad, float *promedio)
+{
+    int retorno = -1;
+    if(cantidad > 0 && promedio != NULL)
+    {
+        *promedio = (float)valorAcumulado / cantidad;
+        retorno = 0;
+    }
+    return retorno;
+
+}
+
+int calcularMaximoMinimo(int valorIngresado, int *maximo, int *minimo)
+{
+    int retorno = -1;
+    if(*maximo != NULL && *minimo != NULL)
+    {
+        if(valorIngresado>*maximo)
+        {
+            *maximo = valorIngresado;
+        }
+        if(valorIngresado<*minimo)
+        {
+            *minimo = valorIngresado;
+        }
+        retorno = 0;
+    }
+
+    return retorno;
+}
+
+
 /*
 
 int sumarNumeros()
